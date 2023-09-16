4f75ee797501f16939b1a26079790bb23908ac9b
Marcelo Henrique <mbbrpiuoficial@gmail.com>
2023-09-16 20:10:52 +0000

    Up

--- app/src/components/logo/logo.jsx
+++ app/src/components/logo/logo.jsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import './Logo.css';
 
-function Logo() {
+function Logo({ textColor, backgroundColor, fontSize, borderColor }) {
+  const logoStyle = {
+    background: backgroundColor || 'transparent',  
+    fontSize: fontSize || '24px',  
+    border: `2px solid ${borderColor || 'transparent'}`, 
+  };
+
   return (
     <div className='logo-bg'>
       <Link to="/">
-        <Box className="logo">
+        <Box className="logo" style={logoStyle}>
           D<span className="colored-letter"></span>EB<span className="colored-letter">V</span>ER
         </Box>
       </Link>
